155b3d3575b80840b81d3337b3c04272174736a7
Jelmergu <github@jelmerwijnja.nl>
2021-10-25 17:37:21 +0000

    Add new Ghosts
    Signed-off-by: Jelmergu <github@jelmerwijnja.nl>

--- constants.js
+++ constants.js
@@ -7,31 +7,31 @@
 // 6 = D.O.T.S Projector
 
 const evidenceTypes = [
-    {
+    { // 0
         name: "EMF Level 5",
         id: "emf"
     },
-    {
+    { // 1
         name: "Spirit Box",
         id: "spiritbox"
     },
-    {
+    { // 2
         name: "Fingerprints",
         id: "fingerprints"
     },
-    {
+    { // 3
         name: "Ghost Orbs",
         id: "ghostorbs"
     },
-    {
+    { // 4
         name: "Ghost Writings",
         id: "ghostwritings"
     },
-    {
+    { // 5
         name: "Freezing Temps",
         id: "freezingtemperatures"
     },
-    {
+    { // 6
         name: "D.O.T.S Projector",
         id: "dotsProjector"
     }
@@ -39,99 +39,123 @@ const evidenceTypes = [
 
 const ghostInfos = [
     {
-        name: "spirit",
+        name: "Spirit",
         evidences: [0, 1, 4],
         description:
-            "<b>EMF Level 5 + Spiritbox + Writings</b><br />No strengths. <span class='weakness'>Smudge sticks will pacify it for a while.</span>",
+            "<b>EMF Level 5 + Spirit Box + Writings</b><br />No strengths.<br /><span class='weakness'>Smudge sticks will pacify it for a while.</span>",
     },
     {
-        name: "wraith",
+        name: "Wraith",
         evidences: [0, 1, 6],
         description:
-            "<b>EMF Level 5 + Spiritbox + Projector</b><br />Floats, footsteps rare. <span class='weakness'>Has toxicity to salt.</span>",
+            "<b>EMF Level 5 + Spirit Box + Projector</b><br />Floats, footsteps rare.<br /><span class='weakness'>Has toxicity to salt.</span>",
     },
     {
-        name: "phantom",
+        name: "Phantom",
         evidences: [1, 2, 6],
         description:
-            "<b>Spiritbox + Fingerprints + Projector</b><br />Seeing it drops sanity fast. <span class='weakness'>Taking its photo makes it hide for a while.</span>",
+            "<b>Spirit Box + Fingerprints + Projector</b><br />Seeing it drops sanity fast.<br /><span class='weakness'>Taking its photo makes it hide for a while.</span>",
     },
     {
-        name: "poltergeist",
+        name: "Poltergeist",
         evidences: [1, 2, 4],
         description:
-            "<b>Spiritbox + Fingerprints + Writings</b><br />Can throw objects around. <span class='weakness'>Ineffective in empty rooms.</span>",
+            "<b>Spirit Box + Fingerprints + Writings</b><br />Can throw objects around.<br /><span class='weakness'>Ineffective in empty rooms.</span>",
     },
     {
-        name: "banshee",
+        name: "Banshee",
         evidences: [2, 3, 6],
         description:
-            "<b>Fingerprints + Orbs + Projector</b><br />Targets lone individuals. <span class='weakness'>Fears crucifix & less aggressive near them.</span>",
+            "<b>Fingerprints + Orbs + Projector</b><br />Targets lone individuals.<br /><span class='weakness'>Fears crucifix & less aggressive near them.</span>",
     },
     {
-        name: "jinn",
+        name: "Jinn",
         evidences: [0, 2, 5],
         description:
-            "<b>EMF Level 5 + Fingerprints + Freezing temps</b><br />Travels fast if victim is far. <span class='weakness'>Kill electricity to stop its powers.</span>",
+            "<b>EMF Level 5 + Fingerprints + Freezing temps</b><br />Travels fast if victim is far.<br /><span class='weakness'>Kill electricity to stop its powers.</span>",
     },
     {
-        name: "mare",
+        name: "Mare",
         evidences: [1, 3, 4],
         description:
-            "<b>Spiritbox + Orbs + Writings</b><br />More chances to attack in darkness. <span class='weakness'>Turn lights on to reduce aggression.</span>",
+            "<b>Spirit Box + Orbs + Writings</b><br />More chances to attack in darkness.<br /><span class='weakness'>Turn lights on to reduce aggression.</span>",
     },
     {
-        name: "revenant",
+        name: "Revenant",
         evidences: [3, 4, 5],
         description:
-            "<b>Orbs + Writings + Freezing temps</b><br />Travels fast when hunting. <span class='weakness'>Moves slowly if it can't see humans.</span>",
+            "<b>Orbs + Writings + Freezing temps</b><br />Travels fast when hunting.<br /><span class='weakness'>Moves slowly if it can't see humans.</span>",
     },
     {
-        name: "shade",
+        name: "Shade",
         evidences: [0, 4, 5],
         description:
-            "<b>EMF Level 5 + Writings + Freezing temps</b><br />Shy/hard to find. Less activity around groups. <span class='weakness'>Can't hunt if multiple people around.</span>",
+            "<b>EMF Level 5 + Writings + Freezing temps</b><br />Shy/hard to find. Less activity around groups.<br /><span class='weakness'>Can't hunt if multiple people around.</span>",
     },
     {
-        name: "demon",
+        name: "Demon",
         evidences: [2, 4, 5],
         description:
-            "<b>Fingerprints + Writings + Freezing temps</b><br />Very aggressive. Attacks often. <span class='weakness'>Ouija board use won't affect sanity.</span>",
+            "<b>Fingerprints + Writings + Freezing temps</b><br />Very aggressive. Attacks often.<br /><span class='weakness'>Ouija board use won't affect sanity.</span>",
     },
     {
-        name: "yurei",
+        name: "Yurei",
         evidences: [3, 5, 6],
         description:
-            "<b>Orbs + Freezing temps + Projector</b><br />Strong effect on sanity. <span class='weakness'>Smudging its room contains it to that room for a while.</span>",
+            "<b>Orbs + Freezing temps + Projector</b><br />Strong effect on sanity.<br /><span class='weakness'>Smudging its room contains it to that room for a while.</span>",
     },
     {
-        name: "oni",
+        name: "Oni",
         evidences: [0, 5, 6],
         description:
-            "<b>EMF Level 5 + Freezing temps + Projector</b><br />More active with people nearby. <span class='weakness'>High activity makes it easier to find and identify.</span>",
+            "<b>EMF Level 5 + Freezing temps + Projector</b><br />More active with people nearby.<br /><span class='weakness'>High activity makes it easier to find and identify.</span>",
     },
     {
-        name: "hantu",
+        name: "Hantu",
         evidences: [2, 3, 5],
         description:
-            "<b>Fingerprints + Orbs + Freezing temps</b><br />Lower temperatures can cause the Hantu to move at faster speeds. <span class='weakness'>A Hantu will move slower in warmer areas.</span>",
+            "<b>Fingerprints + Orbs + Freezing temps</b><br />Lower temperatures can cause the Hantu to move at faster speeds.<br /><span class='weakness'>A Hantu will move slower in warmer areas.</span>",
     },
     {
-        name: "yokai",
+        name: "Yokai",
         evidences: [1, 3, 6],
         description:
-            "<b>Spiritbox + Orbs + Projector</b><br />Talking near a Yokai will anger it and increase it's chance of attacking. <span class='weakness'>When hunting a Yokai can only hear voices close to it.</span>",
+            "<b>Spirit Box + Orbs + Projector</b><br />Talking near a Yokai will anger it and increase it's chance of attacking.<br /><span class='weakness'>When hunting a Yokai can only hear voices close to it.</span>",
     },
     {
         name: "Goryo",
         evidences: [0, 2, 6],
         description:
-            "<b>EMF Level 5 + Fingerprints + D.O.T.S Projector</b><br />Will only show itself on camera when no people are there. <span class='weakness'>Rarely seen far from place of death</span>"
+            "<b>EMF Level 5 + Fingerprints + D.O.T.S Projector</b><br />Will only show itself on camera when no people are there.<br /><span class='weakness'>Rarely seen far from place of death</span>"
     },
     {
         name: "Myling",
         evidences: [0, 2, 4],
         description:
-            "<b>EMF Level 5 + Fingerprints + Ghost Writing</b><br />Quiter when hunting <span class='weakness'>More paranormal sound</span>"
-    }
+            "<b>EMF Level 5 + Fingerprints + Ghost Writing</b><br />Quiter when hunting<br /><span class='weakness'>More paranormal sound</span>"
+    },
+    {
+        name: "Onryo",
+        evidences: [1, 3, 5],
+        description:
+            "<b>Spirit Box + Orbs + Freezing temps</b><br />Can attack after extinguishing a flame<br /><span class='weakness'>Less likely to hunt when threatened</span>"
+    },
+    {
+        name: "The Twins",
+        evidences: [0, 1, 5],
+        description:
+            "<b>EMF Level 5 + Spirit Box + Freezing temps</b><br />Either Twin can be angered and initiate an attack<br /><span class='weakness'>The Twins will often interact with the environment at the same time</span>"
+    },
+    {
+        name: "Raiju",
+        evidences: [0, 3, 6],
+        description:
+            "<b>EMF Level 5 + Orbs + D.O.T.S Projector</b><br />Can siphon power from nearby electrical devices to move faster<br /><span class='weakness'>Easier to track because of the electrical interferance</span>"
+    },
+    {
+        name: "Obake",
+        evidences: [0, 2, 3],
+        description:
+            "<b>EMF Level 5 + Fingerprints + Orbs</b><br />Rarely leaves traces on interaction<br /><span class='weakness'>Might shapeshift leaving behind unique evidence</span>"
+    },
 ];
